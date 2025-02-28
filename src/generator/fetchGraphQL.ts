78ff04271261ac3d6c4c187ee78c3bab05a95b8e
Jurij Master <foto888999@gmail.com>
2025-02-28 16:28:39 +0200

    1

--- src/generator/fetchGraphQL.ts
+++ src/generator/fetchGraphQL.ts
@@ -2,18 +2,20 @@
 export const fetchGraphQL= (params:any) =>{
 
     // const mode='local'
-    const mode:any="global"
+    const mode:any="globalDocker"
+    // const mode:any="globalPHP"
 
     let fetchPath=''
-    const pathLocal = 'http://localhost:8088/graphql.php'
-    // const pathGlobal = 'https://sw-server-przw.onrender.com/graphql.php'
-    const pathGlobal = 'https://site555.antinedoebit.com/app1/html/graphql.php'
 
     switch (mode){
-        case "local": fetchPath=pathLocal; break;
-        default: fetchPath=pathGlobal;
+        case "local": fetchPath='http://localhost:8088/graphql.php'; break;
+        case "globalPHP": fetchPath='https://midnightblue-newt-595842.hostingersite.com/app1/html/graphql.php'; break;
+        case "globalDocker": fetchPath='https://sw-server-przw.onrender.com/graphql.php'; break;
+        default: fetchPath="http://localhost:8088/graphql.php";
     }
-    console.log("mode1 pathLocal1 === ",mode, pathGlobal)
+
+    console.log("=== ▄▄▄▄ fetchPath1 ",mode,fetchPath)
+
     return fetch(fetchPath, {
         method: 'post',
         mode:'cors',
@@ -23,9 +25,9 @@ export const fetchGraphQL= (params:any) =>{
         },
         body: JSON.stringify({query: params.gqlRequest})
     }).then((responseData) => {
-        console.log("=== responseData1",responseData)
+        // console.log("=== responseData1",responseData)
         responseData.json().then((d:any) => {
-            console.log("=== responseData ",params.entityName,d)
+            console.log("▄▄▄▄▄▄▄▄=== responseData ",params.entityName,d)
             params.setDataCallback(d)
         })
     }).catch((e) => {
